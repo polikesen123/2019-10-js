906df14b7ce48ead8ab5dcebd6cfcf48ef4b76e3
polikesen123 <polikesen@163.com>
2019-12-05 23:23:36 +0800

    12-05

--- week2/day06/练习.js
+++ week2/day06/练习.js
@@ -20,19 +20,19 @@ Fn.prototype = {
 };
 var f1 = new Fn;
 var f2 = new Fn;
-console.log(f1.getX === f2.getX);
-console.log(f1.getY === f2.getY);
-console.log(f1.__proto__.getY === Fn.prototype.getY);
-console.log(f1.__proto__.getX === f2.getX);
-console.log(f1.getX === Fn.prototype.getX);
-console.log(f1.constructor);
-console.log(Fn.prototype.__proto__.constructor);
-f1.getX();
-f1.__proto__.getX();
-f2.getY();
-Fn.prototype.getY();
-f1.sum();
-Fn.prototype.sum();
+console.log(f1.getX === f2.getX);//false
+console.log(f1.getY === f2.getY);//true
+console.log(f1.__proto__.getY === Fn.prototype.getY);//true
+console.log(f1.__proto__.getX === f2.getX);//false
+console.log(f1.getX === Fn.prototype.getX);//false
+console.log(f1.constructor);//fn
+console.log(Fn.prototype.__proto__.constructor);//Object类
+f1.getX();//100
+f1.__proto__.getX();//undefined
+f2.getY();//200
+Fn.prototype.getY();//400
+f1.sum();//300
+Fn.prototype.sum();//NaN
 
 // 第二题
 function Foo() {
@@ -44,13 +44,13 @@ Foo.prototype.getName = function () {console.log(3);};
 var getName = function () {console.log(4);};
 function getName() {console.log(5);}
 
-Foo.getName();
-getName();
-Foo().getName(); 
-getName();
-var a = new Foo.getName();
-var b = new Foo().getName();
-var c = new new Foo().getName();
+Foo.getName();//2
+getName();//4
+Foo().getName(); //1
+getName();//1
+var a = new Foo.getName();//2
+var b = new Foo().getName();//3
+var c = new new Foo().getName();//3
 console.log(a,b,c);
 
 // 第三题
@@ -63,8 +63,8 @@ Person.prototype.getName = function () {
 Person.prototype.age = 5000;
 
 var per1 = new Person;
-per1.getName();
+per1.getName();//zhufeng
 per1.age = 9;
-console.log(per1.age);
+console.log(per1.age);//9
 var per2 = new Person;
-console.log(per2.age);
+console.log(per2.age);//5000
